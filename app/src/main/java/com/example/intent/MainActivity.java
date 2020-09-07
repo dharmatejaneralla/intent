071167f633237467d58ee6ca21d9b554d313fac7
dharmatejaneralla <ndteja2000@gmail.com>
2020-09-07 21:38:40 +0530

    final

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.intent;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-Button sendbtn;
+//Button sendbtn;
 EditText sendtext;
+TextView reply;
+    public static int TEXT_REQUEST=1;
     public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,26 @@ EditText sendtext;
         setContentView(R.layout.activity_main);
       //  sendbtn = findViewById(R.id.sendbtn);
         sendtext = findViewById(R.id.etsend);
+        reply = findViewById(R.id.replymsg);
     }
 
     public void sendtect(View view) {
         Intent i = new Intent(this,SecondActivity.class);
         String msg = sendtext.getText().toString();
         i.putExtra(EXTRA_MESSAGE,msg);
-        startActivity(i);
+        startActivityForResult(i,TEXT_REQUEST);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (TEXT_REQUEST== requestCode && resultCode == RESULT_OK)
+        {
+            String replymsg = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+            reply.setText(replymsg);
+            reply.setVisibility(View.VISIBLE);
+
+        }
     }
 }
--- app/src/main/java/com/example/intent/SecondActivity.java
+++ app/src/main/java/com/example/intent/SecondActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 TextView txt;
+EditText etreply;
+public static final String EXTRA_REPLY = "com.example.android.intent.extra.REPLY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,14 @@ TextView txt;
         String msg = i.getStringExtra(MainActivity.EXTRA_MESSAGE);
         txt = findViewById(R.id.text_message);
         txt.setText(msg);
+        etreply = findViewById(R.id.etreply);
+    }
+
+    public void sendreply(View view) {
+        String replymsg = etreply.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY,replymsg);
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
